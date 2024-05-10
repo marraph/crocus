8a116bc1f007aff970c1582ac3a16aceccbcddea
Marius Ahsmus <marius.ahsmus@gmail.com>
2024-05-10 22:52:44 +0200

    updated: searchfield

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -7,8 +7,8 @@ import {Search} from "lucide-react";
 export function SearchField() {
     return (
         <div className={"group h-full flex flex-row items-center rounded-lg bg-black border border-white border-opacity-20 focus:text-white"}>
-            <Search size={18} className={"group-focus:text-white text-placeholder ml-2"} />
-            <Input placeholder={"Search"} className={"m-0 w-60 h-10 focus-visible:ring-0 border-0 bg-none focus-visible:outline-none"}></Input>
+            <Search size={18} className={"group-focus:text-white text-placeholder ml-2 mr-2"} />
+            <Input placeholder={"Search"} className={"m-0 p-0 w-60 h-10 focus-visible:ring-0 border-0 bg-none focus-visible:outline-none"}></Input>
             <div className={"bg-dark rounded-md p-1 mr-2"}>
                 <span className={"flex items-center text-xs text-placeholder"}>{"⌘ K"}</span>
             </div>
