c8eb078439cdd75b26ec6bd16b4be400b12b6320
marius <marius.ahsmus@gmail.com>
2024-05-11 22:07:22 +0200

    updated: page, layout, taskCreateDialog

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {TaskTable} from "@/components/tables/TaskTable";
+import {Button, ButtonIcon} from "@marraph/daisy/components/button/Button";
+import {ListFilter, SquarePen} from "lucide-react";
+import {SwitchButton} from "@marraph/daisy/components/switchbutton/SwitchButton";
+import {TaskCreateDialog} from "@/components/dialogs/TaskCreateDialog";
+
+export default function Home() {
+  return (
+    <div className={"h-full flex flex-col"}>
+        <div className={"w-full flex flex-row pr-8 text-nowrap justify-between"}>
+            <span className={"text-xl ml-8"}>{"Tasks"}</span>
+            <div className={"flex flex-row justify-end space-x-2"}>
+                <TaskCreateDialog></TaskCreateDialog>
+                <SwitchButton firstTitle={"Table"} secondTitle={"Card"}/>
+                <Button text={"Filter"} className={"w-min h-8"}>
+                    <ButtonIcon icon={<ListFilter size={20}/>}/>
+                </Button>
+            </div>
+        </div>
+        <TaskTable></TaskTable>
+    </div>
+  );
+}
--- src/components/dialogs/TaskCreateDialog.tsx
+++ src/components/dialogs/TaskCreateDialog.tsx
@@ -3,18 +3,28 @@
 import {Dialog, DialogContent} from "@marraph/daisy/components/dialog/Dialog";
 import {Input} from "@marraph/daisy/components/input/Input";
 import {Textarea} from "@marraph/daisy/components/textarea/Textarea";
-import React from "react";
+import React, {useState} from "react";
+import {Button, ButtonIcon} from "@marraph/daisy/components/button/Button";
+import {SquarePen} from "lucide-react";
 
 export function TaskCreateDialog() {
+    const [isOpen, setIsOpen] = useState(false)
+
     return(
-        <Dialog>
-            <DialogContent>
-                <div>
-                    <Input placeholder={"Title"}></Input>
-                    <Textarea placeholder={"Description"}></Textarea>
-                </div>
-            </DialogContent>
-        </Dialog>
+        <>
+            <Button text={"Create Task"} theme={"white"} className={"w-min h-8"} onClick={() => setIsOpen(true)}>
+                <ButtonIcon icon={<SquarePen size={20}/>}/>
+            </Button>
+            <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+                <DialogContent>
+                    <div>
+                        <Input placeholder={"Title"}></Input>
+                        <Textarea placeholder={"Description"}></Textarea>
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </>
+
     )
 }
 
