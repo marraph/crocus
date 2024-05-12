7e2388779ed8edc0cbb85cfdd5130446a8ebaf58
Marius Ahsmus <marius.ahsmus@gmail.com>
2024-05-12 21:40:52 +0200

    updated: taskCreateDialog

--- src/components/dialogs/TaskCreateDialog.tsx
+++ src/components/dialogs/TaskCreateDialog.tsx
@@ -8,9 +8,15 @@ import {Button, ButtonIcon} from "@marraph/daisy/components/button/Button";
 import {SquarePen} from "lucide-react";
 import {cn} from "@/utils/cn";
 import {CloseButton} from "@marraph/daisy/components/closebutton/CloseButton";
+import {Combobox, ComboboxItem} from "@marraph/daisy/components/combobox/Combobox";
 
 export const TaskCreateDialog: React.FC<React.HTMLAttributes<HTMLDialogElement>> = ({className, ...props}) => {
     const [isOpen, setIsOpen] = useState(false)
+
+    const team = ["None", "Frontend", "Backend"];
+    const project = ["None", "ServerAPI", "ClientAPI"];
+    const topic = ["None", "Bug", "Feature"];
+    const status = ["None", "Todo", "In Progress", "Done"];
 
     return(
         <>
@@ -23,7 +29,27 @@ export const TaskCreateDialog: React.FC<React.HTMLAttributes<HTMLDialogElement>>
                     <div className={"flex flex-col flex-grow space-y-4"}>
                         <Input placeholder={"Title"} className={""}></Input>
                         <Textarea placeholder={"Description"} className={"h-20"}></Textarea>
-                        <div className={"flex flex-row justify-end"}>
+                        <div className={"flex flex-row justify-end space-x-2"}>
+                            <Combobox buttonTitle={"Team"} className={"h-8"}>
+                                {team.map((team) => (
+                                    <ComboboxItem title={team} key={team}>{team}</ComboboxItem>
+                                ))}
+                            </Combobox>
+                            <Combobox buttonTitle={"Project"} className={"h-8"}>
+                                {project.map((project) => (
+                                    <ComboboxItem title={project} key={project}>{project}</ComboboxItem>
+                                ))}
+                            </Combobox>
+                            <Combobox buttonTitle={"Topic"} className={"h-8"}>
+                                {topic.map((topic) => (
+                                    <ComboboxItem title={topic} key={topic}>{topic}</ComboboxItem>
+                                ))}
+                            </Combobox>
+                            <Combobox buttonTitle={"Status"} className={"h-8"}>
+                                {status.map((status) => (
+                                    <ComboboxItem title={status} key={status}>{status}</ComboboxItem>
+                                ))}
+                            </Combobox>
                             <Button text={"Create"} theme={"white"} className={"w-min h-8"}></Button>
                         </div>
                     </div>
