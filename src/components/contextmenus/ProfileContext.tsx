2d085ddd9faeb9f3ec35bd43a43f5cd60f7b06ca
Marius Ahsmus <marius.ahsmus@gmail.com>
2024-05-13 20:46:24 +0200

    updated: taskCreateDialog, profilecontext

--- src/components/contextmenus/ProfileContext.tsx
+++ src/components/contextmenus/ProfileContext.tsx
@@ -8,14 +8,14 @@ import {cn} from "@/utils/cn";
 export function ProfileContext() {
     return (
         <ContextMenu className={cn("font-normal text-sm")}>
-            <ContextMenuItem title={"My organisation"}>
+            <ContextMenuItem title={"My organisation"} className={"mx-2"}>
                 <ContextMenuIcon icon={<Briefcase size={18}/>} />
             </ContextMenuItem>
-            <ContextMenuItem title={"Settings"}>
+            <ContextMenuItem title={"Settings"} className={"mx-2 mb-2"}>
                 <ContextMenuIcon icon={<Settings size={18}/>} />
             </ContextMenuItem>
-            <ContextMenuSeperator />
-            <ContextMenuItem title={"Log out"} className={"text-lightred hover:text-lightred"}>
+            <ContextMenuSeperator/>
+            <ContextMenuItem title={"Log out"} className={"text-lightred hover:text-lightred mx-2 mt-2"}>
                 <ContextMenuIcon icon={<LogOut size={18}/>} />
             </ContextMenuItem>
         </ContextMenu>
--- src/components/dialogs/TaskCreateDialog.tsx
+++ src/components/dialogs/TaskCreateDialog.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import {Dialog, DialogContent} from "@marraph/daisy/components/dialog/Dialog";
-import {Input} from "@marraph/daisy/components/input/Input";
+import {Dialog, DialogContent, DialogSeperator} from "@marraph/daisy/components/dialog/Dialog";
 import {Textarea} from "@marraph/daisy/components/textarea/Textarea";
 import React, {useState} from "react";
 import {Button, ButtonIcon} from "@marraph/daisy/components/button/Button";
@@ -9,7 +8,6 @@ import {SquarePen} from "lucide-react";
 import {cn} from "@/utils/cn";
 import {CloseButton} from "@marraph/daisy/components/closebutton/CloseButton";
 import {Combobox, ComboboxItem} from "@marraph/daisy/components/combobox/Combobox";
-import {Seperator} from "@marraph/daisy/components/seperator/Seperator";
 
 export const TaskCreateDialog: React.FC<React.HTMLAttributes<HTMLDialogElement>> = ({className, ...props}) => {
     const [isOpen, setIsOpen] = useState(false)
@@ -24,39 +22,41 @@ export const TaskCreateDialog: React.FC<React.HTMLAttributes<HTMLDialogElement>>
             <Button text={"Create Task"} theme={"white"} className={"w-min h-8"} onClick={() => setIsOpen(true)}>
                 <ButtonIcon icon={<SquarePen size={20}/>}/>
             </Button>
-            <Dialog className={cn("border border-white border-opacity-20 top-1/3 left-1/3 w-4/12", className)}  {...props}
+            <Dialog className={cn("border border-white border-opacity-20 top-1/3 left-1/3 w-4/12 drop-shadow-2xl", className)}  {...props}
                     open={isOpen} onClose={() => setIsOpen(false)}>
-                <DialogContent className={"flex flex-row justify-between space-x-4"}>
-                    <div className={"flex flex-col flex-grow space-y-4"}>
-                        <span className={"text-lg text-white"}>{"New Task"}</span>
-                        <input placeholder={"Task Title"} className={"rounded-lg bg-black py-2 text-white placeholder-placeholder focus-visible:ring-0 border-0 focus-visible:outline-none"}/>
-                        <Textarea placeholder={"Add Description..."} className={"h-20 bg-black placeholder-placeholder"}></Textarea>
-                        <div className={"flex flex-row space-x-2"}>
-                            <Combobox buttonTitle={"Team"} className={"h-8"}>
+                <DialogContent className={cn("flex flex-row justify-between space-x-4 px-4 pt-4 pb-2", className)}>
+                    <div className={cn("flex flex-col flex-grow space-y-2", className)}>
+                        <span className={cn("text-lg text-white", className)}>{"New Task"}</span>
+                        <input placeholder={"Task Title"} className={cn("rounded-lg bg-black py-2 text-white placeholder-placeholder focus-visible:ring-0 border-0 focus-visible:outline-none", className)}/>
+                        <Textarea placeholder={"Add Description..."} className={cn("h-20 bg-black placeholder-placeholder resize-none overflow-hidden", className)}></Textarea>
+                        <div className={cn("flex flex-row space-x-2", className)}>
+                            <Combobox buttonTitle={"Team"} className={cn("h-8", className)}>
                                 {team.map((team) => (
                                     <ComboboxItem key={team} title={""}>{team}</ComboboxItem>
                                 ))}
                             </Combobox>
-                            <Combobox buttonTitle={"Project"} className={"text-xs h-8"}>
+                            <Combobox buttonTitle={"Project"} className={cn("h-8", className)}>
                                 {project.map((project) => (
                                     <ComboboxItem title={""} key={project}>{project}</ComboboxItem>
                                 ))}
                             </Combobox>
-                            <Combobox buttonTitle={"Topic"} className={"h-8"}>
+                            <Combobox buttonTitle={"Topic"} className={cn("h-8", className)}>
                                 {topic.map((topic) => (
                                     <ComboboxItem title={""} key={topic}>{topic}</ComboboxItem>
                                 ))}
                             </Combobox>
-                            <Combobox buttonTitle={"Status"} className={"h-8"}>
+                            <Combobox buttonTitle={"Status"} className={cn("h-8", className)}>
                                 {status.map((status) => (
                                     <ComboboxItem title={""} key={status}>{status}</ComboboxItem>
                                 ))}
                             </Combobox>
                         </div>
-                        <Button text={"Create"} theme={"white"} className={"w-min h-8"}></Button>
                     </div>
-                    <CloseButton className={"h-min w-min"} text={""} onClick={() => setIsOpen(false)}></CloseButton>
-
+                    <CloseButton className={cn("h-min w-min", className)} text={""} onClick={() => setIsOpen(false)}></CloseButton>
+                </DialogContent>
+                <DialogSeperator/>
+                <DialogContent className={cn("flex flex-row justify-end px-4 py-0.5", className)}>
+                    <Button text={"Create"} theme={"white"} className={cn("w-min h-8", className)}></Button>
                 </DialogContent>
             </Dialog>
         </>
