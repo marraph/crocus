2f1c6c28243c1fdcd95125e18d692006c6841146
marius <marius.ahsmus@gmail.com>
2024-05-18 20:47:18 +0200

    feat: taskId page

--- src/app/tasks/[id]/page.tsx
+++ src/app/tasks/[id]/page.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import {Breadcrump} from "@marraph/daisy/components/breadcrump/Breadcrump";
 import {Seperator} from "@marraph/daisy/components/seperator/Seperator";
+import {useRouter} from "next/navigation";
+import {Button} from "@marraph/daisy/components/button/Button";
+import {CheckCheck, MessageSquare, Trash2, Wrench} from "lucide-react";
 
 const task = {
     title: "Title",
@@ -9,6 +14,7 @@ const task = {
     priority: "Priority",
     topic: "Topic",
     status: "Status",
+    dueDate: "DueDate",
     change: "Change",
     changedAt: "ChangedAt",
     changedFrom: "ChangedFrom",
@@ -17,67 +23,92 @@ const task = {
 }
 
 export default function TaskIdPage() {
-    return (
-        <div className={"h-full flex flex-col items-stretch space-y-4 px-8 pb-8"}>
-            <Breadcrump pastText={"Tasks"} nowText={"Api not working"}/>
-            <div className={"flex flex-row grow border border-white border-opacity-20 bg-black rounded-lg"}>
-                <div className={"w-full "}>
+    const router = useRouter();
 
+    return (
+        <div className={"h-full flex flex-col space-y-4 px-8 pb-8"}>
+            <div className={"w-full flex flex-row justify-between"}>
+                <Breadcrump pastText={"Tasks"} nowText={"Api not working"} className={"h-8"}
+                            onClick={() => router.push("/tasks/")}/>
+                <div className={"flex flex-row space-x-2"}>
+                    <Button text={"Message"} theme={"white"} className={"h-8"}>
+                        <MessageSquare size={20} className={"mr-3"}/>
+                    </Button>
+                    <Button text={"Edit"} className={"h-8"}>
+                        <Wrench size={20} className={"mr-3"}/>
+                    </Button>
+                    <Button text={"Close"} className={"h-8"}>
+                        <CheckCheck size={20} className={"mr-3"}/>
+                    </Button>
+                    <Button text={""} className={"h-8 text-lightred " +
+                        "hover:text-lightred hover:bg-lightred hover:bg-opacity-10"}>
+                        <Trash2 size={20}/>
+                    </Button>
                 </div>
 
-                <div className={"w-max justify-end bg-dark rounded-lg h-min flex flex-col grow text-sm"}>
+            </div>
+
+            <div className={"flex flex-row grow border border-white border-opacity-20 bg-black rounded-lg"}>
+                <div className={"w-full min-h-full bg-dark rounded-l-lg"}>
+
+                </div>
+                <div className={"w-max min-h-full bg-black rounded-lg h-min flex flex-col text-sm"}>
                     <div className={"flex flex-row space-x-4 px-4 pt-4 pb-2"}>
-                        <span className={"text-gray"}>Title</span>
+                        <div className={"w-16 text-gray"}>Title</div>
                         <span>{task.title}</span>
                     </div>
                     <Seperator className={"w-full py-4"}/>
-                    <div className={"flex flex-row space-x-4 px-4 py-2"}>
-                        <span className={"text-gray"}>Desc.</span>
-                        <span>{task.description}</span>
+                    <div className={"flex flex-row space-x-4 px-4 py-2 h-32"}>
+                        <div className={"w-16 text-gray"}>Description</div>
+                        <span className={"flex-1 break-words"}>{task.description}</span>
                     </div>
                     <Seperator className={"w-full"}/>
                     <div className={"flex flex-row space-x-4 px-4 py-2"}>
-                        <span className={"text-gray"}>Team</span>
+                        <div className={"w-16 text-gray"}>Team</div>
                         <span>{task.team}</span>
                     </div>
                     <div className={"flex flex-row space-x-4 px-4 py-2"}>
-                        <span className={"text-gray"}>Project</span>
+                        <div className={"w-16 text-gray"}>Project</div>
                         <span>{task.project}</span>
                     </div>
                     <div className={"flex flex-row space-x-4 px-4 py-2"}>
-                        <span className={"text-gray"}>Priority</span>
+                        <div className={"w-16 text-gray"}>Priority</div>
                         <span>{task.priority}</span>
                     </div>
                     <div className={"flex flex-row space-x-4 px-4 py-2"}>
-                        <span className={"text-gray"}>Topic</span>
+                        <div className={"w-16 text-gray"}>Topic</div>
                         <span>{task.topic}</span>
                     </div>
                     <div className={"flex flex-row space-x-4 px-4 py-2"}>
-                        <span className={"text-gray"}>Status</span>
+                        <div className={"w-16 text-gray"}>Status</div>
                         <span>{task.status}</span>
+                    </div>
+                    <div className={"flex flex-row space-x-4 px-4 py-2"}>
+                        <div className={"w-16 text-gray"}>Due Date</div>
+                        <span>{task.dueDate}</span>
                     </div>
                     <Seperator className={"w-full py-4"}/>
                     <span className={"text-xs text-placeholder px-4 py-2"}>LAST CHANGE</span>
                     <div className={"flex flex-row space-x-4 px-4 py-2"}>
-                        <span className={"text-gray"}>Change</span>
+                        <div className={"w-24 text-gray"}>Change</div>
                         <span>{task.change}</span>
                     </div>
                     <div className={"flex flex-row space-x-4 px-4 py-2"}>
-                        <span className={"text-gray"}>ChangedAt</span>
+                        <div className={"w-24 text-gray"}>Time Changed</div>
                         <span>{task.changedAt}</span>
                     </div>
                     <div className={"flex flex-row space-x-4 px-4 py-2"}>
-                        <span className={"text-gray"}>ChangedFrom</span>
+                        <div className={"w-24 text-gray"}>Changer</div>
                         <span>{task.changedFrom}</span>
                     </div>
                     <Seperator className={"w-full py-4"}/>
                     <span className={"text-xs text-placeholder px-4 py-2"}>CREATION</span>
                     <div className={"flex flex-row space-x-4 px-4 py-2"}>
-                        <span className={"text-gray"}>CreatedAt</span>
+                        <div className={"w-24 text-gray"}>Time Created</div>
                         <span>{task.createdAt}</span>
                     </div>
-                    <div className={"flex flex-row space-x-4 px-4 py-2"}>
-                        <span className={"text-gray"}>Creator</span>
+                    <div className={"flex flex-row space-x-4 px-4 pt-2 pb-4"}>
+                        <div className={"w-24 text-gray"}>Creator</div>
                         <span>{task.creator}</span>
                     </div>
                 </div>
