4e3badbe50a06bad496b7222337d49b64055badc
marius <marius.ahsmus@gmail.com>
2024-05-19 12:06:53 +0200

    feat: drawer navItem adjust

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import {NavigationItem} from "@marraph/daisy/components/navigationitem/NavigationItem";
-import React from "react";
+import React, {useState} from "react";
 import {CalendarDays, ClipboardList, LayoutDashboard, SquarePlus, Timer} from "lucide-react";
 import {cn} from "@/utils/cn";
 import {ProfileContext} from "@/components/contextmenus/ProfileContext";
@@ -10,24 +10,33 @@ import {useRouter} from "next/navigation";
 export function Drawer() {
     const router = useRouter();
 
+    const [selectDashbaord, setSelectDashboard] = useState(false);
+    const [selectTasks, setSelectTasks] = useState(false);
+    const [selectTime, setSelectTime] = useState(false);
+    const [selectCalendar, setSelectCalendar] = useState(false);
+
     return (
         <div className={cn("w-min h-screen flex flex-col justify-between bg-black pt-4 pr-5 pl-4")}>
             <div className={"space-y-2"}>
                 <div className={"space-y-1"}>
                     <span className={cn("text-placeholder text-xs px-1")}>{"MENU"}</span>
-                    <NavigationItem title={"Dashboard"} onClick={() => router.push("/dashboard")}
+                    <NavigationItem selected={selectDashbaord} title={"Dashboard"}
+                                    onClick={() => {router.push("/dashboard"); setSelectDashboard(true)}}
                                     icon={<LayoutDashboard size={18}/>}/>
-                    <NavigationItem title={"Tasks"} onClick={() => router.push("/tasks")}
+                    <NavigationItem selected={selectTasks} title={"Tasks"}
+                                    onClick={() => {router.push("/tasks"); setSelectTasks(true)}}
                                     icon={<ClipboardList size={18}/>}/>
-                    <NavigationItem title={"Timetracking"} onClick={() => router.push("/timetracking")}
+                    <NavigationItem selected={selectTime} title={"Timetracking"}
+                                    onClick={() => {router.push("/timetracking"); setSelectTime(true)}}
                                     icon={<Timer size={18}/>}/>
-                    <NavigationItem title={"Calendar"} onClick={() => router.push("/calendar")}
+                    <NavigationItem selected={selectCalendar} title={"Calendar"}
+                                    onClick={() => {router.push("/calendar"); setSelectCalendar(true)}}
                                     icon={<CalendarDays size={18}/>}/>
                 </div>
 
                 <div className={"py-12"}>
                     <span className={cn("text-placeholder text-xs px-1")}>{"TEAMS"}</span>
-                    <NavigationItem title={"Join a team"} icon={<SquarePlus size={18}/>}/>
+                    <NavigationItem selected={false} title={"Join a team"} icon={<SquarePlus size={18}/>}/>
                 </div>
             </div>
             <ProfileContext/>
