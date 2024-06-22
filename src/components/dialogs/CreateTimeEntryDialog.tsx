fd67a32fafa63f2efb7efde35ce389463b99a556
marius <marius.ahsmus@gmail.com>
2024-06-22 22:14:07 +0200

    feat: createTimeEntry validation

--- src/components/dialogs/CreateTimeEntryDialog.tsx
+++ src/components/dialogs/CreateTimeEntryDialog.tsx
@@ -3,7 +3,7 @@ import {Button} from "@marraph/daisy/components/button/Button";
 import {cn} from "@/utils/cn";
 import {Dialog, DialogRef} from "@marraph/daisy/components/dialog/Dialog";
 import {Alert, AlertContent, AlertIcon, AlertRef, AlertTitle} from "@marraph/daisy/components/alert/Alert";
-import {AlarmClockPlus, BookCopy, ClipboardList} from "lucide-react";
+import {AlarmClockPlus, BookCopy, ClipboardList, Clock2, Clock8} from "lucide-react";
 import {CloseButton} from "@marraph/daisy/components/closebutton/CloseButton";
 import {Seperator} from "@marraph/daisy/components/seperator/Seperator";
 import { useUser } from "@/context/UserContext";
@@ -17,10 +17,13 @@ export const CreateTimeEntryDialog = React.forwardRef<HTMLDialogElement, React.D
     const alertRef = useRef<AlertRef>(null);
     const taskRef = useRef<SearchSelectRef>(null);
     const projectRef = useRef<SearchSelectRef>(null);
+    const timeFromRef = useRef<SearchSelectRef>(null);
+    const timeToRef = useRef<SearchSelectRef>(null);
     const switchRef = useRef<SwitchRef>(null);
     const [comment, setComment] = useState("");
     const [projectSelected, setProjectSelected] = useState<string | null>(null);
     const [taskSelected, setTaskSelected] = useState<string | null>(null);
+    const [update, setUpdate] = useState<number>(0);
     const {data:user, isLoading:userLoading, error:userError} = useUser();
 
     const getProjects = () => {
@@ -73,6 +76,42 @@ export const CreateTimeEntryDialog = React.forwardRef<HTMLDialogElement, React.D
         return tasks;
     }
 
+    const times = [
+        "12:00AM", "12:15AM", "12:30AM", "12:45AM",
+        "01:00AM", "01:15AM", "01:30AM", "01:45AM",
+        "02:00AM", "02:15AM", "02:30AM", "02:45AM",
+        "03:00AM", "03:15AM", "03:30AM", "03:45AM",
+        "04:00AM", "04:15AM", "04:30AM", "04:45AM",
+        "05:00AM", "05:15AM", "05:30AM", "05:45AM",
+        "06:00AM", "06:15AM", "06:30AM", "06:45AM",
+        "07:00AM", "07:15AM", "07:30AM", "07:45AM",
+        "08:00AM", "08:15AM", "08:30AM", "08:45AM",
+        "09:00AM", "09:15AM", "09:30AM", "09:45AM",
+        "10:00AM", "10:15AM", "10:30AM", "10:45AM",
+        "11:00AM", "11:15AM", "11:30AM", "11:45AM",
+        "12:00PM", "12:15PM", "12:30PM", "12:45PM",
+        "01:00PM", "01:15PM", "01:30PM", "01:45PM",
+        "02:00PM", "02:15PM", "02:30PM", "02:45PM",
+        "03:00PM", "03:15PM", "03:30PM", "03:45PM",
+        "04:00PM", "04:15PM", "04:30PM", "04:45PM",
+        "05:00PM", "05:15PM", "05:30PM", "05:45PM",
+        "06:00PM", "06:15PM", "06:30PM", "06:45PM",
+        "07:00PM", "07:15PM", "07:30PM", "07:45PM",
+        "08:00PM", "08:15PM", "08:30PM", "08:45PM",
+        "09:00PM", "09:15PM", "09:30PM", "09:45PM",
+        "10:00PM", "10:15PM", "10:30PM", "10:45PM",
+        "11:00PM", "11:15PM", "11:30PM", "11:45PM"
+    ];
+
+    const validateInput = () => {
+        let timeFrom = timeFromRef.current?.getSelectedValue();
+        let timeTo = timeToRef.current?.getSelectedValue();
+        if (comment.trim() === "" && !projectSelected && !taskSelected) return true;
+        if (!times.includes(timeFrom as string) || !times.includes(timeTo as string) ||
+            times.indexOf(timeFrom as string) >= times.indexOf(timeTo as string)) return true;
+        return false;
+    }
+
     const createTimeEntry = () => {
         if (switchRef.current?.getValue() === false) {
             dialogRef.current?.close();
@@ -82,6 +121,8 @@ export const CreateTimeEntryDialog = React.forwardRef<HTMLDialogElement, React.D
         setTaskSelected(null);
         taskRef.current?.reset();
         projectRef.current?.reset();
+        timeFromRef.current?.reset();
+        timeToRef.current?.reset();
         alertRef.current?.show();
     }
 
@@ -113,6 +154,8 @@ export const CreateTimeEntryDialog = React.forwardRef<HTMLDialogElement, React.D
         setTaskSelected(null);
         taskRef.current?.reset();
         projectRef.current?.reset();
+        timeFromRef.current?.setValue("09:00AM");
+        timeToRef.current?.setValue("09:00AM");
         switchRef.current?.setValue(false);
     }
 
@@ -133,8 +176,9 @@ export const CreateTimeEntryDialog = React.forwardRef<HTMLDialogElement, React.D
                               onChange={(e) => setComment(e.target.value)} value={comment}>
                     </Textarea>
 
-                    <div className={"flex flex-row items-center space-x-2 px-4 z-50"}>
-                        <SearchSelect buttonTitle={"Project"} ref={projectRef} icon={<BookCopy size={16}/>} size={"small"}>
+                    <div className={"flex flex-row items-center space-x-2 px-4 py-2"}>
+                        <SearchSelect buttonTitle={"Project"} ref={projectRef} width={100}
+                                      icon={<BookCopy size={16}/>} size={"small"} className={"z-50"}>
                             {!taskSelected && getProjects().map((project) => (
                                 <SearchSelectItem key={project} title={project} onClick={() => handleProjectChange(project)}></SearchSelectItem>
                             ))}
@@ -143,12 +187,28 @@ export const CreateTimeEntryDialog = React.forwardRef<HTMLDialogElement, React.D
                             }
                         </SearchSelect>
 
-                        <SearchSelect buttonTitle={"Task"} ref={taskRef} icon={<ClipboardList size={16}/>} size={"small"}>
+                        <SearchSelect buttonTitle={"Task"} ref={taskRef} width={100}
+                                      icon={<ClipboardList size={16}/>} size={"small"} className={"z-50"}>
                             {!projectSelected && getAllTasks().map((task) => (
                                 <SearchSelectItem key={task} title={task} onClick={() => handleTaskChange(task)}></SearchSelectItem>
                             ))}
                             {projectSelected && getTasks(projectSelected).map((task) => (
                                 <SearchSelectItem key={task} title={task} onClick={() => handleTaskChange(task)}></SearchSelectItem>
+                            ))}
+                        </SearchSelect>
+                    </div>
+
+                    <div className={"flex flex-row items-center space-x-2 px-4 pb-2"}>
+                        <SearchSelect buttonTitle={"From"} preSelectedValue={"09:00AM"} ref={timeFromRef} width={100}
+                                      icon={<Clock2 size={16}/>} size={"small"} className={"z-40"}>
+                            {times.map((time) => (
+                                <SearchSelectItem key={time} title={time} onClick={() => setUpdate(update+1)}></SearchSelectItem>
+                            ))}
+                        </SearchSelect>
+                        <SearchSelect buttonTitle={"To"} preSelectedValue={"09:00AM"} ref={timeToRef} width={100}
+                                      icon={<Clock8 size={16}/>} size={"small"} className={"z-40"}>
+                            {times.map((time) => (
+                                <SearchSelectItem key={time} title={time} onClick={() => setUpdate(update+1)}></SearchSelectItem>
                             ))}
                         </SearchSelect>
                     </div>
@@ -159,7 +219,8 @@ export const CreateTimeEntryDialog = React.forwardRef<HTMLDialogElement, React.D
                             <span>{"Create more"}</span>
                             <Switch ref={switchRef}></Switch>
                         </div>
-                        <Button text={"Create"} theme={"white"} onClick={createTimeEntry} disabled={comment.trim() === "" && !projectSelected && !taskSelected}
+                        <Button text={"Create"} theme={"white"} onClick={createTimeEntry}
+                                disabled={validateInput()}
                                 className={cn("w-min h-8 disabled:cursor-not-allowed disabled:hover:none disabled:bg-dark disabled:text-gray", className)}>
                         </Button>
                     </div>
