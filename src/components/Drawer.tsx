6a7b406254770534850cfcf8004d5a7bb7ec0642
marius <marius.ahsmus@gmail.com>
2024-07-17 10:57:49 +0200

    feat: searchDialog rework

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -2,19 +2,22 @@
 
 import {NavigationItem, useNavigation} from "@marraph/daisy/components/navigationitem/NavigationItem";
 import React, {useEffect, useRef} from "react";
-import {CalendarDays, ClipboardList, LayoutDashboard, Moon, SquarePlus, Timer} from "lucide-react";
+import {CalendarDays, ClipboardList, LayoutDashboard, Moon, Search, SquarePlus, Timer} from "lucide-react";
 import {cn} from "@/utils/cn";
 import {ProfileContextMenu} from "@/components/contextmenus/ProfileContextMenu";
 import {usePathname, useRouter} from "next/navigation";
-import {SearchField} from "@/components/SearchField";
+import {SearchDialog} from "@/components/dialogs/SearchDialog";
 import {DialogRef} from "@marraph/daisy/components/dialog/Dialog";
 import {JoinTeamDialog} from "@/components/dialogs/JoinTeamDialog";
+import {Input} from "@marraph/daisy/components/input/Input";
+import {Shortcut} from "@marraph/daisy/components/shortcut/Shortcut";
 
 export const Drawer = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({className, ...props}, ref) => {
     const router = useRouter();
     const pathSegments = usePathname().split('/');
     const { selectedItem, setSelectedItem } = useNavigation();
     const joinTeamDialogRef = useRef<DialogRef>(null);
+    const searchDialogRef = useRef<DialogRef>(null);
 
     useEffect(() => {
         if (pathSegments.includes('dashboard')) {
@@ -33,6 +36,7 @@ export const Drawer = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTML
     return (
         <>
             <JoinTeamDialog ref={joinTeamDialogRef}/>
+            <SearchDialog ref={searchDialogRef}/>
 
             <div className={cn("w-min h-screen flex flex-col justify-between bg-black pt-4 pr-5 pl-4")} {...props}>
                 <div className={"space-y-2"}>
@@ -40,27 +44,38 @@ export const Drawer = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTML
                         <Moon size={30}/>
                         <span className={"text-3xl"}>Luna</span>
                     </div>
-                    <SearchField/>
+
+                    <div
+                        className={"group flex flex-row justify-between items-center space-x-1 rounded-lg bg-black border border-edge focus:text-white pr-1"}
+                        onClick={() => searchDialogRef.current?.showModal()}>
+                        <div className={"flex flex-row items-center"}>
+                            <Search size={18} className={"group-focus:text-white text-marcador ml-2 mr-2"}/>
+                            <Input placeholder={"Search"} border={"none"}
+                                   className={"w-max text-sm m-0 p-0 h-8 bg-black"}></Input>
+                        </div>
+                        <Shortcut text={"⌘ K"} className={"justify-end"}/>
+                    </div>
+
                     <div className={"space-y-1 pt-4"}>
                         <span className={cn("text-marcador text-xs px-1")}>{"MENU"}</span>
                         <NavigationItem selected={selectedItem === "Dashboard"}
                                         title={"Dashboard"}
-                                        onClick={() =>  router.push("/dashboard")}
+                                        onClick={() => router.push("/dashboard")}
                                         icon={<LayoutDashboard size={18}/>}/>
 
                         <NavigationItem selected={selectedItem === "Tasks"}
                                         title={"Tasks"}
-                                        onClick={() =>  router.push("/tasks")}
+                                        onClick={() => router.push("/tasks")}
                                         icon={<ClipboardList size={18}/>}/>
 
                         <NavigationItem selected={selectedItem === "Timetracking"}
                                         title={"Timetracking"}
-                                        onClick={() =>  router.push("/timetracking")}
+                                        onClick={() => router.push("/timetracking")}
                                         icon={<Timer size={18}/>}/>
 
                         <NavigationItem selected={selectedItem === "Calendar"}
                                         title={"Calendar"}
-                                        onClick={() =>  router.push("/calendar")}
+                                        onClick={() => router.push("/calendar")}
                                         icon={<CalendarDays size={18}/>}/>
                     </div>
 
@@ -76,6 +91,6 @@ export const Drawer = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTML
                 <ProfileContextMenu/>
             </div>
         </>
-  );
+    );
 });
 Drawer.displayName = "Drawer";
---
+++ src/components/dialogs/SearchDialog.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import React, {forwardRef} from "react";
+import {Input} from "@marraph/daisy/components/input/Input";
+import {
+    CalendarPlus,
+    ClipboardList,
+    CornerDownLeft,
+    MoveDown,
+    MoveUp,
+    Search,
+    Settings,
+    SquarePen,
+    Timer
+} from "lucide-react";
+import {DialogRef} from "@marraph/daisy/components/dialog/Dialog";
+import {useOutsideClick} from "@marraph/daisy/utils/clickOutside";
+import {Shortcut} from "@marraph/daisy/components/shortcut/Shortcut";
+import {Seperator} from "@marraph/daisy/components/seperator/Seperator";
+import {mutateRef} from "@/utils/mutateRef";
+
+interface DialogProps extends React.DialogHTMLAttributes<HTMLDialogElement> {
+}
+
+export const SearchDialog = forwardRef<DialogRef, DialogProps>(({className, ...props}, ref) => {
+    const dialogRef = mutateRef(ref);
+
+    const outsideClickRef = useOutsideClick(() => {
+        dialogRef?.current?.close();
+    });
+
+    return (
+        <div className={"flex items-center justify-center"}>
+            <dialog
+                className={"group backdrop:bg-black/60 backdrop backdrop-opacity-20 backdrop-brightness-0 rounded-lg bg-black overflow-visible border border-edge"} {...props}
+                ref={dialogRef ?? undefined}
+                style={{width: 700}}
+            >
+                <div ref={outsideClickRef}>
+                    <div className={"flex flex-row items-center"}>
+                        <Search size={18}
+                                className={"text-marcador ml-4 mr-2"}
+                        />
+                        <Input placeholder={"Search or type a command"}
+                               border={"none"}
+                               className={"w-full text-md m-0 mr-2 p-0 h-12 bg-black"}
+                        />
+                    </div>
+                    <Seperator/>
+                    <div className={"text-marcador text-sm px-4 pt-4"}>
+                        <span>{"Quick Actions"}</span>
+                    </div>
+                    <div className={"flex flex-col items-center px-2 py-1 font-normal cursor-pointer"}>
+                        <div
+                            className={"flex flex-row items-center justify-between w-full rounded-lg text-gray hover:text-white hover:bg-dark"}>
+                            <div className={"flex flex-row items-center space-x-2 ml-2 my-2"}>
+                                <SquarePen size={18}/>
+                                <span>Create new task</span>
+                            </div>
+                            <div className={"flex flex-row space-x-2 items-center text-xs text-gray mr-2"}>
+                                <span>{"Command"}</span>
+                                <Shortcut text={"⌘ T"}/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={"flex flex-col items-center px-2 py-1 font-normal cursor-pointer"}>
+                        <div
+                            className={"flex flex-row items-center justify-between w-full rounded-lg text-gray hover:text-white hover:bg-dark"}>
+                            <div className={"flex flex-row items-center space-x-2 ml-2 my-2"}>
+                                <CalendarPlus size={18}/>
+                                <span>Create new appointment</span>
+                            </div>
+                            <div className={"flex flex-row space-x-2 items-center text-xs text-gray mr-2"}>
+                                <span>{"Command"}</span>
+                                <Shortcut text={"⌘ P"}/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={"flex flex-col items-center px-2 pt-1 pb-2 font-normal cursor-pointer"}>
+                        <div
+                            className={"flex flex-row items-center justify-between w-full rounded-lg text-gray hover:text-white hover:bg-dark"}>
+                            <div className={"flex flex-row items-center space-x-2 ml-2 my-2"}>
+                                <Timer size={18}/>
+                                <span>Track your time</span>
+                            </div>
+                            <div className={"flex flex-row space-x-2 items-center text-xs text-gray mr-2"}>
+                                <span>{"Command"}</span>
+                                <Shortcut text={"⌘ F"}/>
+                            </div>
+                        </div>
+                    </div>
+                    <Seperator/>
+                    <div className={"text-placeholder text-sm px-4 pt-4"}>
+                        <span>{"Recent"}</span>
+                    </div>
+                    <div className={"flex flex-col items-center px-2 py-1 font-normal cursor-pointer"}>
+                        <div
+                            className={"flex flex-row items-center justify-between w-full rounded-lg text-gray hover:text-white hover:bg-dark"}>
+                            <div className={"flex flex-row items-center space-x-2 ml-2 my-2"}>
+                                <ClipboardList size={18}/>
+                                <span>{"Task: Server API doesn't work"}</span>
+                            </div>
+                            <div className={"flex flex-row space-x-2 items-center text-xs text-gray mr-2"}>
+                                <span>{"Command"}</span>
+                                <Shortcut text={"⌘ E"}/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={"flex flex-col items-center px-2 pt-1 pb-2 font-normal cursor-pointer"}>
+                        <div
+                            className={"flex flex-row items-center justify-between w-full rounded-lg text-gray hover:text-white hover:bg-dark"}>
+                            <div className={"flex flex-row items-center space-x-2 ml-2 my-2"}>
+                                <Settings size={18}/>
+                                <span>Settings</span>
+                            </div>
+                            <div className={"flex flex-row space-x-2 items-center text-xs text-gray mr-2"}>
+                                <span>{"Command"}</span>
+                                <Shortcut text={"⌘ S"}/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <Seperator/>
+
+                    <div className={"flex flex-row items-center space-x-8 px-4 h-12 text-gray text-sm"}>
+                        <div className={"flex flex-row items-center space-x-2 cursor-pointer"}>
+                            <span>{"Close"}</span>
+                            <Shortcut text={"ESC"}/>
+                        </div>
+                        <div className={"flex flex-row items-center space-x-2 cursor-pointer"}>
+                            <span>{"Select"}</span>
+                            <Shortcut>
+                                <CornerDownLeft size={16}/>
+                            </Shortcut>
+                        </div>
+                        <div className={"flex flex-row items-center space-x-1 cursor-pointer"}>
+                            <span className={"mr-1"}>{"Navigate"}</span>
+                            <Shortcut>
+                                <MoveUp size={16}/>
+                            </Shortcut>
+                            <Shortcut>
+                                <MoveDown size={16}/>
+                            </Shortcut>
+                        </div>
+                    </div>
+                </div>
+            </dialog>
+        </div>
+    );
+});
+SearchDialog.displayName = "SearchDialog";
