fe57894e5fc8edd5b124d817438c0d5499ae5343
Marius Ahsmus <marius.ahsmus@gmail.com>
2024-07-29 17:35:16 +0200

    feat: filter key logic implemented

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -3,7 +3,7 @@
 import React, {ReactNode, useState} from "react";
 import {ContextMenu, ContextMenuItem} from "@marraph/daisy/components/contextmenu/ContextMenu";
 import {Button} from "@marraph/daisy/components/button/Button";
-import {Box, ChevronDown, ChevronUp, CircleAlert, LineChart, Tag, Users} from "lucide-react";
+import {Box, CircleAlert, LineChart, ListFilter, Tag, Users} from "lucide-react";
 import {useOutsideClick} from "@marraph/daisy/utils/clickOutside";
 import {CloseButton} from "@marraph/daisy/components/closebutton/CloseButton";
 
@@ -24,14 +24,86 @@ interface FilterProps {
 
 
 const Filter: React.FC<FilterProps> = ({ title, items, ...props }) => {
-    const [isOpen, setOpen] = useState(false);
-    const [openMenu, setOpenMenu] = useState<FilterItem | null>(null);
-    const [filters, setFilters] = useState<SelectedFilter[] | null>(null);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [subMenuOpen, setSubMenuOpen] = useState<FilterItem | null>(null);
+    const [filters, setFilters] = useState<SelectedFilter[]>([]);
 
     const menuRef = useOutsideClick(() => {
-        setOpen(false);
+        setMenuOpen(false);
     });
 
+    const handleMenuClick = (value: string) => {
+        if (subMenuOpen) {
+            const newFilter = { name: subMenuOpen.name, value: value };
+            const updatedFilters = filters.filter(f => f.name !== newFilter.name);
+            setFilters([...updatedFilters, newFilter]);
+        }
+        setSubMenuOpen(null);
+    }
+
+    return (
+        <div className={"flex flex-col space-y-10"}>
+            <div className={"flex flex-row space-x-2 items-center"}>
+                <Button text={title}
+                        icon={<ListFilter size={16} className={"mr-2"}/>}
+                        onClick={() => {
+                            if (subMenuOpen) setMenuOpen(false);
+                            else setMenuOpen(!menuOpen);
+                            setSubMenuOpen(null);
+                        }}
+                        {...props}
+                />
+                {filters && filters.map((filter, index) => (
+                    <FilterBadge
+                        key={index}
+                        name={filter.name}
+                        value={filter.value}
+                        onClick={() => setFilters(filters.filter(f => f !== filter))}
+                    />
+                ))}
+            </div>
+
+            {
+                <>
+                    {menuOpen &&
+                        <ContextMenu className={"fixed text-xs w-max py-1 shadow-2xl z-50"} ref={menuRef} >
+                            {items.map((item, index) => (
+                                <ContextMenuItem
+                                    key={index}
+                                    title={item.name}
+                                    onClick={() => {
+                                        setSubMenuOpen(item);
+                                        setMenuOpen(false);
+                                    }}
+                                />
+                            ))}
+                        </ContextMenu>
+                    }
+
+                    {subMenuOpen &&
+                        <ContextMenu className={"fixed text-xs w-max py-1 shadow-2xl z-50"} ref={menuRef}>
+                            {subMenuOpen.values.map((value, index) => (
+                                <ContextMenuItem
+                                    key={index}
+                                    title={value}
+                                    onClick={() => handleMenuClick(value)}
+                                />
+                            ))}
+                        </ContextMenu>
+                    }
+                </>
+            }
+        </div>
+    );
+}
+
+interface FilterBadgeProps {
+    name: string;
+    value: string;
+    onClick: () => void;
+}
+
+const FilterBadge: React.FC<FilterBadgeProps> = ({ name, value, onClick }) => {
     const icons: { [key: string]: ReactNode } = {
         Team: <Users size={16}/>,
         Project: <Box size={16}/>,
@@ -41,68 +113,18 @@ const Filter: React.FC<FilterProps> = ({ title, items, ...props }) => {
     };
 
     return (
-        <div className={"flex flex-col space-y-10"}>
-            <div className={"flex flex-row space-x-2 items-center"}>
-                <Button text={title}
-                        onClick={() => {
-                            if (openMenu) setOpen(false);
-                            else setOpen(!isOpen);
-                            setOpenMenu(null);
-                        }}
-                        icon={isOpen ? <ChevronUp size={16} className={"mr-2"}/> : <ChevronDown size={16} className={"mr-2"}/>}
-                        {...props}
-                />
-                {filters && filters.map((filter, index) => (
-                    <div key={index} className={"h-6 flex flex-row items-center space-x-1 pl-2 bg-edge rounded-lg"}>
-                        {icons[filter.name]}
-                        <span className={"text-xs text-white"}>{filter.name + ":"}</span>
-                        <span className={"text-xs text-white-dark"}>{filter.value}</span>
-                        <CloseButton className={"bg-edge hover:bg-edge"}
-                                     onClick={() => {
-                                        setFilters(filters.filter(f => f !== filter));
-                                     }}
-                        />
-                    </div>
-                ))}
-            </div>
-
-            {
-                <>
-                    {isOpen &&
-                        <ContextMenu className={"fixed text-xs w-max py-1 shadow-2xl z-50"} ref={menuRef} >
-                            {items.map((item, index) => (
-                                <ContextMenuItem key={index}
-                                                 title={item.name}
-                                                 onClick={() => {
-                                                     setOpen(false);
-                                                     setOpenMenu(item);
-                                                 }}
-                                />
-                            ))}
-                        </ContextMenu>
-                    }
-
-                    {openMenu &&
-                        <ContextMenu className={"fixed text-xs w-max py-1 shadow-2xl z-50"} ref={menuRef}>
-                            {openMenu.values.map((value, index) => (
-                                <ContextMenuItem key={index}
-                                                 title={value}
-                                                 onClick={() => {
-                                                     setOpenMenu(null);
-                                                     setFilters([...(filters || []), {name: openMenu.name, value: value}]);
-                                                 }}
-                                />
-                            ))}
-                        </ContextMenu>
-                    }
-                </>
-            }
-
+        <div className={"h-6 flex flex-row items-center space-x-1 pl-2 bg-edge rounded-lg"}>
+            {icons[name]}
+            <span className={"text-xs text-white"}>{name + ":"}</span>
+            <span className={"text-xs text-white-dark"}>{value}</span>
+            <CloseButton className={"bg-edge hover:bg-edge"}
+                         onClick={onClick}
+            />
         </div>
     );
 }
 
-export { Filter };
+export {Filter};
 
 
 
