65986e1210d43baded5f7fdeb9cb2133dcf867b6
Raphael <raphael.goetz.7@gmail.com>
2024-08-15 09:15:04 +0200

    feat: added topic service & types

---
+++ src/action/topic.ts
@@ -0,0 +1,37 @@
+import {topic} from "@/schema";
+import {db} from "@/database/drizzle";
+import {eq, InferInsertModel, InferSelectModel} from "drizzle-orm";
+
+export type Topic = InferSelectModel<typeof topic>
+export type NewTopic = InferInsertModel<typeof topic>
+export type UpdateTopic = Partial<NewTopic>
+
+export const createTopic = async (newTopic: NewTopic): Promise<Topic> => {
+    const [createdTopic] = await db
+        .insert(topic)
+        .values(newTopic)
+        .returning()
+
+    return createdTopic
+}
+
+export const updateTopic = async (id: number, updateTopic: UpdateTopic): Promise<Topic> => {
+    const [updatedTopic] = await db
+        .update(topic)
+        .set(updateTopic)
+        .where(eq(topic.id, id))
+        .returning()
+
+    return updatedTopic
+}
+
+export const deleteTopic = async (id: number) => {
+    await db.delete(topic).where(eq(topic.id, id))
+}
+
+export const getTopicFromTeam = async (teamId: number): Promise<Topic[]> => {
+    return db
+        .select()
+        .from(topic)
+        .where(eq(topic.teamId, teamId));
+}
