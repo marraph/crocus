5d115d2fa7b8bc1978f80303fe76fd172d2acd9b
Raphael <raphael.goetz.7@gmail.com>
2024-08-23 11:13:15 +0200

    feat: added enum types

---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+type Priority = 'low' | 'medium' | 'high';
+type State = 'pending' | 'planing' | 'started' | 'tested' | 'finished';
+type AbsenceReason = 'sick' | 'vacation';
+
+export type {
+    Priority,
+    State,
+    AbsenceReason
+}
