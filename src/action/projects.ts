5a2cb2a5bcc35e8c14765523adb204283712529e
Raphael <raphael.goetz.7@gmail.com>
2024-08-24 12:24:24 +0200

    feat: added function to get projects from user

--- src/action/projects.ts
+++ src/action/projects.ts
@@ -1,5 +1,6 @@
-import {project, task} from "@/schema";
+import {members, project, task} from "@/schema";
 import {
+    ActionResult,
     createEntry,
     deleteEntity,
     Entity,
@@ -9,6 +10,8 @@ import {
     UpdateEntity,
     updateEntry
 } from "@/action/actions";
+import {eq} from "drizzle-orm";
+import {db} from "@/database/drizzle";
 
 type Project = Entity<typeof project>
 type NewProject = NewEntity<typeof project>
@@ -34,6 +37,44 @@ const getProjectsFromTeam = async (
     limit: number = 100
 ) => queryEntity(project, teamId, project.teamId, limit)
 
+
+const getProjectsFromUser = async (
+    userId: number,
+    limit: number = 100
+): Promise<ActionResult<Project[]>> => {
+    try {
+
+        const projects = await db
+            .select({
+                id: project.id,
+                name: project.name,
+                description: project.description,
+                priority: project.priority,
+                isArchived: project.isArchived,
+                createdAt: project.createdAt,
+                updatedAt: project.updatedAt,
+                createdBy: project.createdBy,
+                updatedBy: project.updatedBy,
+                teamId: project.teamId
+            })
+            .from(project)
+            .innerJoin(members, eq(project.teamId, members.teamId))
+            .where(eq(members.userId, userId))
+            .limit(limit)
+
+        if (!projects || projects.length == 0) {
+            return {success: false, error: 'No projects found!'}
+        }
+
+        return {success: true, data: projects}
+    } catch (err) {
+        const error = err as Error
+        return {success: false, error: error.message}
+    }
+
+}
+
+
 export type {
     Project,
     NewProject,
@@ -45,5 +86,6 @@ export {
     createProject,
     updateProject,
     deleteProject,
-    getProjectsFromTeam
+    getProjectsFromTeam,
+    getProjectsFromUser
 }
