3baadb810c55b37804622ca6cc98092a653d2cb5
Raphael <raphael.goetz.7@gmail.com>
2024-08-26 20:54:00 +0200

    fix: added missing tables to schema definition

--- src/database/drizzle.ts
+++ src/database/drizzle.ts
@@ -1,6 +1,36 @@
 import {drizzle} from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import {
+    absences, entries, organisationMemberRelations,
+    organisationMembers, organisationRelations,
+    organisations, projectRelations,
+    projects, taskRelations,
+    tasks, teamMemberRelations,
+    teamMembers, teamRelations,
+    teams,
+    topics,
+    users
+} from "@/schema";
 
 const queryClient = postgres('postgresql://avnadmin:AVNS_NV0uRHW6UnKC_k_NdLD@marraph-1-speed.j.aivencloud.com:22482/calla2?sslmode=require')
 
-export const db = drizzle(queryClient);
+export const db = drizzle(queryClient, {
+    schema: {
+        users,
+        topics,
+        tasks,
+        projects,
+        organisations,
+        teams,
+        teamMembers,
+        organisationMembers,
+        absences,
+        entries,
+        organisationRelations,
+        teamRelations,
+        projectRelations,
+        taskRelations,
+        organisationMemberRelations,
+        teamMemberRelations
+    }
+});
