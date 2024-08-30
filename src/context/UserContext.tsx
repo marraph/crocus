d10a1c2b5791f3774d0ee0218849518b80b478e9
Raphael <raphael.goetz.7@gmail.com>
2024-08-30 21:42:23 +0200

    feat: abstracted action usage

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,17 +1,9 @@
 "use client";
 
 import React, {createContext, ReactNode, useContext, useEffect, useState} from 'react';
-import {queryUser, updateUser, UpdateUser, User} from "@/action/user";
-import {
-    createOrganisation,
-    deleteOrganisation,
-    NewOrganisation, Organisation,
-    updateOrganisation,
-    UpdateOrganisation
-} from "@/action/organisation";
-import {createTeam, deleteTeam, NewTeam, Team, updateTeam, UpdateTeam} from "@/action/team";
+import {queryUser} from "@/action/user";
 import {ActionResult} from "@/action/actions";
-import {CompletedUser} from "@/types/types";
+import {ActionConsumerType, CompletedUser} from "@/types/types";
 import {userConfig} from "@/database/configurations/userQuery";
 
 interface UserContextType {
@@ -19,17 +11,17 @@ interface UserContextType {
     loading: boolean
     error: string | null;
 
-    actions: {
-        createOrganisation: typeof createOrganisation
-        updateOrganisation: typeof updateOrganisation
-        deleteOrganisation: typeof deleteOrganisation
+    actionConsumer: (action: ActionConsumer) => {}
+}
 
-        createTeam: typeof createTeam
-        updateTeam: typeof updateTeam
-        deleteTeam: typeof deleteTeam
+interface UserProviderProps {
+    children: ReactNode;
+    id: number;
+}
 
-        updateUser: typeof updateUser
-    };
+interface ActionConsumer {
+    consumer: () => Promise<ActionResult<ActionConsumerType>>
+    handler: (currentUser: CompletedUser, input: ActionConsumerType) => CompletedUser
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
@@ -41,12 +33,6 @@ export const useUser = (): UserContextType => {
     }
     return context;
 };
-
-interface UserProviderProps {
-    children: ReactNode;
-    id: number;
-}
-
 
 export const UserProvider: React.FC<UserProviderProps> = async ({children, id}) => {
     const [user, setUser] = useState<CompletedUser | null>(null);
@@ -64,81 +50,22 @@ export const UserProvider: React.FC<UserProviderProps> = async ({children, id}) 
         fetchData();
     }, [id, error, loading]);
 
-    const actions = {
-        createOrganisation: async (newOrganisation: NewOrganisation): Promise<ActionResult<Organisation>> => {
-            const createResult = await createOrganisation(newOrganisation);
+    const actionConsumer = async (action: ActionConsumer) => {
+        if (!user) return
 
-            if (!createResult.success) {
-                //     setError(prev => ({...prev, organisations: createResult.error}));
-                return {success: false, error: createResult.error};
-            }
+        const actionResult = await action.consumer()
 
-            //    setUserData(prev => [...prev, createResult.data]);
-            return {success: true, data: createResult.data};
-        },
-        updateOrganisation: async (id: number, newOrganisation: UpdateOrganisation): Promise<ActionResult<Organisation>> => {
-            const updateResult = await updateOrganisation(id, newOrganisation);
-            if (!updateResult.success) {
-                //     setError(prev => ({...prev, organisations: updateResult.error}));
-                return {success: false, error: updateResult.error};
-            }
-
-            //setUserData(prev => prev.map(o => o.id === id ? updateResult.data : o));
-            return {success: true, data: updateResult.data};
-        },
-        deleteOrganisation: async (id: number): Promise<ActionResult<boolean>> => {
-            const deleteResult = await deleteOrganisation(id);
-            if (!deleteResult.success) {
-                //      setError(prev => ({...prev, organisations: deleteResult.error}));
-                return {success: false, error: deleteResult.error};
-            }
-
-            //  setUserData(prev => prev.filter(o => o.id !== id));
-            return {success: true, data: true};
-        },
-        createTeam: async (newTeam: NewTeam): Promise<ActionResult<Team>> => {
-            const createResult = await createTeam(newTeam);
-            if (!createResult.success) {
-                //    setError(prev => ({...prev, teams: createResult.error}));
-                return {success: false, error: createResult.error};
-            }
-
-            // setUserData(prev => [...prev, createResult.data]);
-            return {success: true, data: createResult.data};
-        },
-        updateTeam: async (id: number, newTeam: UpdateTeam): Promise<ActionResult<Team>> => {
-            const updateResult = await updateTeam(id, newTeam);
-            if (!updateResult.success) {
-                //   setError(prev => ({...prev, teams: updateResult.error}));
-                return {success: false, error: updateResult.error};
-            }
-
-            //   setUserData(prev => prev.map(t => t.id === id ? updateResult.data : t));
-            return {success: true, data: updateResult.data};
-        },
-        deleteTeam: async (id: number): Promise<ActionResult<boolean>> => {
-            const deleteResult = await deleteTeam(id);
-            if (!deleteResult.success) {
-                //     setError(prev => ({...prev, teams: deleteResult.error}));
-                return {success: false, error: deleteResult.error};
-            }
-            // setUserData(prev => prev.filter(t => t.id !== id));
-            return {success: true, data: true};
-        },
-        updateUser: async (id: number, newUser: UpdateUser): Promise<ActionResult<User>> => {
-            const updateResult = await updateUser(id, newUser);
-            if (!updateResult.success) {
-                //     setError(prev => ({...prev, user: updateResult.error}));
-                return {success: false, error: updateResult.error};
-            }
-
-            // setUser(updateResult.data);
-            return {success: true, data: updateResult.data};
+        if (!actionResult.success) {
+            setError(actionResult.error)
+            return
         }
-    };
+
+        const completedUser = action.handler(user, actionResult.data)
+        setUser(completedUser)
+    }
 
     return (
-        <UserContext.Provider value={{user, loading, error, actions}}>
+        <UserContext.Provider value={{user, loading, error, actionConsumer}}>
             {children}
         </UserContext.Provider>
     );
